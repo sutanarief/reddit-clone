24ba53855c6b4bdea4b5c36f1d5f1f38d416c0bd
sutanarief <sutan1412@gmail.com>
2022-12-02 17:28:02 +0700

    add communities button

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -7,7 +7,8 @@ import {
   MenuItem,
   Flex,
   Icon,
-  MenuDivider
+  MenuDivider,
+  Text
 } from "@chakra-ui/react"
 import { signOut, User } from "firebase/auth"
 import { FaRedditSquare } from "react-icons/fa"
@@ -20,7 +21,6 @@ import React from "react"
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
 
-
 type UserMenuProps = {
   user?: User | null
 }
@@ -29,7 +29,12 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
   const setAuthModalState = useSetRecoilState(authModalState)
   return (
     <Menu>
-      <MenuButton>
+      <MenuButton
+        cursor="pointer"
+        padding="0px 6px"
+        borderRadius={4}
+        _hover={{ outline: "1px solid", outlineColor: "gray.200"}}
+        >
         <Flex align="center">
           <Flex align="center">
             {user ? (
@@ -40,6 +45,21 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
                   color="gray.300"
                   as={FaRedditSquare}
                 />
+                <Flex
+                  direction="column"
+                  display={{base: "none", lg: "flex"}}
+                  fontSize="8pt"
+                  align="flex-start"
+                  mr={8}
+                >
+                  <Text fontWeight={800}>
+                    {user?.displayName || user?.email?.split("@")[0]}
+                  </Text>
+                  <Flex>
+                    <Icon as={IoSparkles} color="brand.100" mr={1} />
+                    <Text color="gray.400">1 karma</Text>
+                  </Flex>
+                </Flex>
               </>
             ) : (
               <Icon fontSize={24} color="gray.400" mr={1} as={VscAccount} />
